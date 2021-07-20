8d1b6c6f3427938567361b581b1ec23c3ced7cb7
Mykhailo Kurshakov <Mykhailo_Kurshakov@epam.com>
2021-07-20 15:08:30 +0300

    Add publish to InfluxDB

--- SeleniumTest1/FirefoxTest.cs
+++ SeleniumTest1/FirefoxTest.cs
@@ -6,37 +6,30 @@ using System.Threading.Tasks;
 using OpenQA.Selenium.Firefox;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using SeleniumTest1.InfluxDB;
+using SeleniumTest1.Pages;
 
 namespace SeleniumTest1
 {
-    class FirefoxTest
+    class FirefoxTest : TestBase
     {
-        IWebDriver driver;
-        string Url = "https://www.wikipedia.org/";
-
-        [SetUp]
-        public void startBrowser()
-        {
-            driver = new FirefoxDriver();
-
-            driver.Manage().Window.Maximize();
-
-            driver.Url = Url;
-        }
-
         [Test]
         public void TestSearchWiki()
         {
-            IWebElement searchField = driver.FindElement(By.XPath("//input[@id=\"searchInput\"]"));
+            /*IWebElement searchField = driver.FindElement(By.XPath("//input[@id=\"searchInput\"]"));
             searchField.SendKeys("automation testing");
             IWebElement searchButton = driver.FindElement(By.XPath("//*[@id=\"search-form\"]/fieldset/button"));
-            searchButton.Click();
-        }
+            searchButton.Click()*/
 
-        [TearDown]
-        public void closeBrowser()
-        {
-            driver.Close();
+            /*var ResponseTime = Convert.ToInt32(js.ExecuteScript("return window.performance.timing.loadEventEnd-window.performance.timing.navigationStart;"));
+            Console.WriteLine("Page {0} loading time is {1} ms", driver.Title, ResponseTime);*/
+
+
+            WikiSearchPage wikiSearchPage = new WikiSearchPage(driver);
+            wikiSearchPage.FillSearchField("automation testing");
+            MetricsWriter.MeasurePageLoad("Wikipedia", "Search", () => wikiSearchPage.searchButton.Click());
         }
     }
 }
